c6493274872f435a85f34adccde489be1dc0b8ef
unknown <tmdeoans@snu.ac.kr>
2020-06-22 22:19:49 +0900

    asdf

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Fragment, Component} from "react";
+import styled from "styled-components"
+
+const ABC = styled.input`
+
+border: dashed;
+`;
+class App extends Component {
+
+
+
+    render () {
+        return (
+            <Fragment>
+             <ABC></ABC>
+            </Fragment>
+        
+        )
+
+        
+    
+    }
+}
+
+export default App;
